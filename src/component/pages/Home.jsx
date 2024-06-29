55e88392a9809cb42afde51a1cefa2c03c382ecd
haseeb <haseeb.ashraf@psca.gop.pk>
2024-06-29 18:07:07 +0500

    home page grid 2nd page

--- src/component/pages/Home.jsx
+++ src/component/pages/Home.jsx
@@ -1,19 +1,135 @@
 import React from 'react'
 import SideBar from '../layout/SideBar'
-import { Box, Typography } from '@mui/material'
+import { Box, Stack, Typography } from '@mui/material'
 import NavBar from '../layout/NavBar'
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
 const Home = () => {
   return (
     <>
 
-      <NavBar/>  
-      <Box height={50} />       
+      <NavBar />
+      <Box height={60} />
       <Box sx={{ display: 'flex' }}>
+
         <SideBar />
 
         <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-          <h1>Home</h1>
+
+          {/* home page main section start */}
+
+          <Grid container spacing={2}>
+            <Grid item xs={8}>
+
+              <Stack spacing={2} direction="row">
+
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Lizards are a widespread group of squamate reptiles, with over 6,000
+                      species, ranging across all continents except Antarctica
+                    </Typography>
+                  </CardContent>
+                </Card>
+
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      Lizards are a widespread group of squamate reptiles, with over 6,000
+                      species, ranging across all continents except Antarctica
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Stack>
+
+
+            </Grid>
+
+            <Box height={20} />
+
+            <Grid item xs={4}>
+
+
+              <Stack spacing={2}>
+
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Lizard
+                    </Typography>
+
+                  </CardContent>
+                </Card>
+
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Lizard
+                    </Typography>
+
+                  </CardContent>
+                </Card>
+              </Stack>
+            </Grid>
+
+          </Grid>
+
+          {/* <hr /> */}
+          <Box height={30} />
+
+
+
+          <Grid container spacing={2}>
+
+            <Grid item xs={8}>
+
+
+              <Card sx={{ maxWidth: 345 }}>
+                <CardContent>
+
+
+                </CardContent>
+              </Card>
+
+
+
+            </Grid>
+
+
+            <Grid item xs={4}>
+
+              <Stack spacing={2}>
+
+
+
+                <Card sx={{ maxWidth: 345 }}>
+                  <CardContent>
+
+
+                  </CardContent>
+                </Card>
+              </Stack>
+
+
+            </Grid>
+
+
+
+          </Grid>
+
+
+
+
+
+          {/* home page main section end */}
 
         </Box>
       </Box>
